8d3d236af36965ee887b0087cc8ec84af5065823
Tim Jarratt <tjarratt@gmail.com>
2017-10-15 21:00:14 +0200

    Add application cache
    This should allow for it to be cacheable offline

--- frontend/scripts/deploy.sh
+++ frontend/scripts/deploy.sh
@@ -7,14 +7,16 @@ cd $(dirname $0)/..
 scripts/build.sh
 
 rm -rf tmp/*
-mkdir tmp/app
+mkdir -p tmp/app/assets
 cp -r Staticfile nginx tmp
 cp index.html tmp/app
 
 timestamp=$(date +"%s")
-cp assets/index.js tmp/app/index-${timestamp}.js
-cp assets/elm-init.js tmp/app/elm-init-${timestamp}.js
+cp assets/index.js tmp/app/assets/index-${timestamp}.js
+cp assets/elm-init.js tmp/app/assets/elm-init-${timestamp}.js
+cp assets/application.appcache tmp/app/assets/application.appcache
 sed -i '' "s/index.js/index-${timestamp}.js/" tmp/app/index.html
 sed -i '' "s/elm-init.js/elm-init-${timestamp}.js/" tmp/app/index.html
+sed -i '' "s/VERSION/${timestamp}/" tmp/app/assets/application.appcache
 
-cf push
+#cf push
