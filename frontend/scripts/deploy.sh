dc317be6f65243a4600aaea55dd45f32c356be1e
Tim Jarratt <tjarratt@pivotal.io>
2018-01-02 14:57:44 +0100

    Version the appcache, not the assets
    Yeah, so I'm just going to link this here and maybe express some
    regrets. Ragrets.
    
    https://alistapart.com/article/application-cache-is-a-douchebag
    
    Basically, If your cache includes an asset, then the browser is going
    to assume that asset will continue to exist forever. When we versioned
    assets, the next time a deploy occurred, a browser that had previously
    visited the site would attempt to fetch the resources for the old
    deploy, which naturally would not exist on our instance. Now we keep the
    file names the same and allow the browser to decide when and if it
    should update.
    
    Signed-off-by: Tim Jarratt <tjarratt@pivotal.io>

--- frontend/scripts/deploy.sh
+++ frontend/scripts/deploy.sh
@@ -11,17 +11,14 @@ mkdir -p tmp/app/assets
 cp -r Staticfile nginx tmp
 cp index.html tmp/app
 
-timestamp=$(date +"%s")
-cp assets/index.js tmp/app/assets/index-${timestamp}.js
-cp assets/index.css tmp/app/assets/index-${timestamp}.css
-cp assets/elm-init.js tmp/app/assets/elm-init-${timestamp}.js
-cp assets/application.appcache tmp/app/assets/application.appcache
-sed -i '' "s/index.js/index-${timestamp}.js/" tmp/app/index.html
-sed -i '' "s/index.css/index-${timestamp}.css/" tmp/app/index.html
-sed -i '' "s/elm-init.js/elm-init-${timestamp}.js/" tmp/app/index.html
-sed -i '' "s/VERSION/${timestamp}/" tmp/app/assets/application.appcache
+# move all of our assets into the deploy directory
+cp assets/* assets/*.css tmp/app/assets/
 
-cp assets/drapeau_francais_retina.png tmp/app/assets
-cp assets/drapeau_francais_favicon.ico tmp/app/favicon.ico
+# version our application cache
+# if we version the assets (javascript, css, etc...) then the browser will
+# freakout when they go away, so its a much better practice to just have a
+# version string in the appcache manifest itself
+timestamp=$(date +"%s")
+sed -i '' "s/VERSION/${timestamp}/" tmp/app/assets/application.appcache
 
 cf push
