8f792ca355ac6e4612b71cc9dce7a8b08d9d7894
Tim Jarratt <tjarratt@gmail.com>
2018-01-06 15:07:33 +0100

    Pass args to the build script
    Useful if you want to run the app with the debugger

--- frontend/scripts/build.sh
+++ frontend/scripts/build.sh
@@ -4,5 +4,5 @@ set -ex
 
 elm-format src tests --yes
 elm-css src/Stylesheets.elm --output=assets
-elm-make src/App.elm --yes --output=assets/index.js
+elm-make src/App.elm --yes --output=assets/index.js $@
 npx postcss assets/index.css --use autoprefixer -d assets
